96af30871886e85d2ab648fd3eeb2ada5ccad38d
IsaacGluck <isaacbgluck@gmail.com>
2014-05-27 18:19:45 -0400

    commented

--- test/Yahoo.java
+++ test/Yahoo.java
@@ -5,22 +5,22 @@ import java.net.URLConnection;
 
 public class Yahoo {
 
-	private static String baseURL = "http://download.finance.yahoo.com/d/quotes.csv?s=";
-	private static String baseURLend = "&f=ab&e=.csv";
+	private static String baseURL = "http://download.finance.yahoo.com/d/quotes.csv?s="; // base url for the api
+	private static String baseURLend = "&f=ab&e=.csv"; // ab are the codes for ask and bid
 
 	public static void getData(String ticker) {
 		try {
-			URL interWebs = new URL(baseURL + ticker + baseURLend);
-			URLConnection interwebConnect = interWebs.openConnection();
-			InputStreamReader isr = new InputStreamReader(interwebConnect.getInputStream());
-			BufferedReader in = new BufferedReader(isr);
+			URL interWebs = new URL(baseURL + ticker + baseURLend); // create a URL instance of the API
+			URLConnection interwebConnect = interWebs.openConnection(); // make a URL connection out of the URL
+			InputStreamReader isr = new InputStreamReader(interwebConnect.getInputStream()); // Retrieve the csv into a InputStreamReader
+			BufferedReader in = new BufferedReader(isr); // For efficiency, thank you oracle API
 			System.out.println( baseURL + ticker + baseURLend );
 
 			System.out.println();
 
-			System.out.println(in.readLine());
+			System.out.println(in.readLine()); // Print the ask and bid about ticker
 		} catch (IOException e) {
-			System.out.println(e);
+			System.out.println(e); // this should be more verbose
 		}
 
 
