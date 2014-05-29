022d9951e35408a1c23a47189f75dcdd8b9ecb95
IsaacGluck <isaacbgluck@gmail.com>
2014-05-29 14:05:14 -0400

    gg

--- test/Yahoo.java
+++ test/Yahoo.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.Proxy;
 
 public class Yahoo {
 
@@ -12,7 +13,11 @@ public class Yahoo {
 	public static void getData(String ticker) {
 		try {
 			URL interWebs = new URL(baseURL + ticker + baseURLend); // create a URL instance of the API
-			URLConnection interwebConnect = interWebs.openConnection(); // make a URL connection out of the URL
+			System.out.println("1st");
+
+			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("149.89.1.30", 3128));
+			
+			URLConnection interwebConnect = interWebs.openConnection(proxy); // make a URL connection out of the URL
 			InputStreamReader isr = new InputStreamReader(interwebConnect.getInputStream()); // Retrieve the csv into a InputStreamReader
 			BufferedReader in = new BufferedReader(isr); // For efficiency, thank you oracle API
 			System.out.println( baseURL + ticker + baseURLend );
@@ -20,8 +25,8 @@ public class Yahoo {
 			System.out.println();
 
 			System.out.println(in.readLine()); // Print the ask and bid about ticker
-		} catch (IOException e) {
-			System.out.println(e); // this should be more verbose
+		} catch (Exception e) {
+		    System.out.println(e); // this should be more verbose
 		}
 
 
