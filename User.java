d918908662f88b61ed88d5ea42e331a5519bcbcc
CobyGoldbergs <CobyGoldbergs@users.noreply.github.com>
2014-05-30 14:46:46 -0400

    User.java created

---
+++ User.java
@@ -0,0 +1,15 @@
+//Skeleton
+
+public class User{
+
+    private ArrayList<Stock> portfolio;
+
+    public User(){
+	portfolio = new ArrayList<Stock>();
+    }
+    
+    public ArrayList<Stock> getPortfolio(){
+	return portfolio;
+    }
+
+}
