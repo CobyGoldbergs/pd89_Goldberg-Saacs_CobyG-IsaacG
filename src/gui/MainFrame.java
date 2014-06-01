ff7898653745bc218a7206c91fb8ea69cb76c5a8
IsaacGluck <isaacbgluck@gmail.com>
2014-06-01 14:24:36 -0400

    GRAPSH ARE A THING

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -5,7 +5,7 @@ import javax.swing.SwingUtilities;
 public class MainFrame extends JFrame{
     
     private JPanel pane; // Screen Holder
-    private HomeScreen home;
+    // private HomeScreen home;
     private ViewStockScreen viewStocks;
     
     public MainFrame() {
@@ -21,11 +21,11 @@ public class MainFrame extends JFrame{
 	pane = new JPanel();
 	pane.setLayout(new CardLayout()); // create the main window holder and make it a deck
 	
-	home = new HomeScreen();
+	// home = new HomeScreen();
 	viewStocks = new ViewStockScreen();
 
 	// put the screens in the deck
-	pane.add(home, "home");
+	// pane.add(home, "home");
 	pane.add(viewStocks, "view");
 	
 	pane.requestFocus(); // make sure the pane has the focus not anything behind it
--- src/gui/ViewStockScreen.java
+++ src/gui/ViewStockScreen.java
@@ -2,14 +2,26 @@ import java.io.*;
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
+import org.math.plot.*;
 
 public class ViewStockScreen extends JPanel {
-    
+    Plot2DPanel plot;
 
     public ViewStockScreen() {
-	setPreferredSize(new Dimension(1000, 650)); // set it to the same size as the container
-	setLayout(null); // it will be a card in the main deck
-	setBackground(Color.black);
+		setPreferredSize(new Dimension(1000, 650)); // set it to the same size as the container
+		setLayout(null); // it will be a card in the main deck
+		setBackground(Color.black);
+
+		plot = new Plot2DPanel();
+		double[] x = { 1, 2, 3, 4, 5, 6 };
+		double[] y = { 45, 89, 6, 32, 63, 12 };
+		plot.addLinePlot("Test Plot", x, y);
+		plot.setBorder(BorderFactory.createTitledBorder("Test Plot"));
+		plot.setBounds(10,50, 980, 590);
+
+		add(plot);
+		revalidate();
+		repaint();
     }
     
 }
