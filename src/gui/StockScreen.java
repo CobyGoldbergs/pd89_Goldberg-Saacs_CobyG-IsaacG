219f7fefa7dc1b80baa9470a887f12e806fa0561
IsaacGluck <isaacbgluck@gmail.com>
2014-06-03 20:31:38 -0400

    almost done with all screens

--- src/gui/StockScreen.java
+++ src/gui/StockScreen.java
@@ -5,23 +5,38 @@ import javax.swing.*;
 import org.math.plot.*;
 
 public class StockScreen extends JPanel {
-    Plot2DPanel plot; // from jmathplots, it extends JPanel
-    JLabel stats;
-    JLabel news;
+    private Plot2DPanel plot; // from jmathplots, it extends JPanel
+    private JButton backButton;
+    private JLabel tickerLabel;
+    private JLabel stats;
+    private JLabel news;
 
-    public StockScreen() {
+    public StockScreen() { // eventually should take a ticker argument and display it accordingly
 		setPreferredSize(new Dimension(1000, 650)); // set it to the same size as the container
 		setLayout(null); // it will be a card in the main deck
 		setBackground(Color.black);
 
+		// Set up the ticker label
+		tickerLabel = new JLabel("<html> <h1> <i>Ticker</i> </h1> </html>"); // should use ticker
+		tickerLabel.setBackground(Color.black);
+		tickerLabel.setForeground(Color.white);
+		add(tickerLabel); // add to panel
+		tickerLabel.setBounds(10, 15, 100, 20);
+		tickerLabel.setOpaque(true);
+
+		// Set up button to view stocks page
+		backButton = new JButton("Back");
+		add(backButton);
+		backButton.setBounds(890, 10, 100, 30);
+
 		// Set up graph
-		//plot = new Plot2DPanel();
-		//double[] x = { 1, 2, 3, 4, 5, 6 };
-		//double[] y = { 45, 89, 6, 32, 63, 12 };
-		//plot.addLinePlot("Test Plot", x, y);
-		//plot.setBorder(BorderFactory.createTitledBorder("Test Plot"));
-		//plot.setBounds(10,50, 980, 590);
-		//add(plot);
+		plot = new Plot2DPanel();
+		double[] x = { 1, 2, 3, 4, 5, 6 }; //getX(ticker)
+		double[] y = { 45, 89, 6, 32, 63, 12 }; //getY(ticker)
+		plot.addLinePlot("Test Stock Plot", x, y); // create line plot from jmathplots
+		plot.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLoweredBevelBorder(), "Test Stock Plot"));
+		plot.setBounds(170,50, 820, 390);
+		add(plot); // add to main jpanel
 
 		//Set up stats label
 		String statsText = "<html><h1 align='center'; style='padding:5';> STATS </h1>"; // add the text to the stats panel
@@ -44,13 +59,6 @@ public class StockScreen extends JPanel {
 		add(news); // add and position news panel
 		news.setBounds(10, 450, 980, 190);
 		news.setOpaque(true);
-
-		
-
-
-
-		revalidate();
-		repaint();
     }
     
 }
---
+++ src/gui/ViewsStockScreen.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class StockScreen extends JPanel {
+    private JButton homeButton;
+    private JLabel viewStocksLabel;
+
+    public StockScreen() { // eventually should take a ticker argument and display it accordingly
+		setPreferredSize(new Dimension(1000, 650)); // set it to the same size as the container
+		setLayout(null); // it will be a card in the main deck
+		setBackground(Color.black);
+
+		// Set up the ticker label
+		viewStocksLabel = new JLabel("<html> <h1> <i>View Stocks</i> </h1> </html>"); // should use ticker
+		viewStocksLabel.setBackground(Color.black);
+		viewStocksLabel.setForeground(Color.white);
+		add(viewStocksLabel); // add to panel
+		viewStocksLabel.setBounds(10, 15, 175, 20);
+		viewStocksLabel.setOpaque(true);
+
+		// Set up button to view stocks page
+		backButton = new JButton("Home");
+		add(backButton);
+		backButton.setBounds(890, 10, 100, 30);
+    }
+    
+}
